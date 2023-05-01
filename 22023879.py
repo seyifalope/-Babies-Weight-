43dc95f6275d86190fbf5a393c07fd6c3e843b9c
seyifalope <126199071+seyifalope@users.noreply.github.com>
2023-05-01 12:03:42 +0100

    Add files via upload
    python code providing answers to the questions on babies weight

---
+++ 22023879.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  1 11:18:38 2023
+
+@author: SEYI FALOPE
+"""
+
+# Importing necessary libraries for analysis
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Reading data from a csv file into an array
+
